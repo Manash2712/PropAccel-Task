d596b82fe61959f09e00ed47952d21e50b34c023
Manash2712 <manashchauhan2712@gmail.com>
2023-07-01 00:38:26 +0530

    added carousel

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -8,93 +8,162 @@ const Review = () => {
                     typesetting industry. Lorem Ipsum has been the industry
                 </p>
             </div>
-            <div className="flex px-[175px] justify-between flex-wrap mt-8 gap-4">
-                <div className="h-[238px] w-[465px] px-10 pt-10 border border-gray-400 rounded-lg">
-                    <div className="flex">
-                        <div>
-                            <img src="src\assets\Lolla 1.png" alt="" />
-                        </div>
-                        <div className="ml-6">
-                            <h4 className="text[16px] font-bold">
-                                Lolla Smith
-                            </h4>
-                            <p className="text-[14px]">Microsoft</p>
-                            <p className="flex gap-2">
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                            </p>
+            <div className="px-[175px]">
+                <div className="carousel w-full flex px-[175px] mt-8 gap-4">
+                    <div
+                        id="item1"
+                        className="h-[208px] w-[465px] carousel-item px-10 pt-10 shadow-lg shadow-slate-400 border border-gray-400 rounded-lg"
+                    >
+                        <div className="">
+                            <div className="flex">
+                                <div className="h-[70px] w-[70px]">
+                                    <img src="src\assets\Lolla 1.png" alt="" />
+                                </div>
+                                <div className="ml-6">
+                                    <h4 className="text[16px] font-bold">
+                                        Lolla Smith
+                                    </h4>
+                                    <p className="text-[14px]">Microsoft</p>
+                                    <p className="flex gap-2">
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <p className="text-[12px]">
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry`s standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make
+                                </p>
+                            </div>
                         </div>
                     </div>
-                    <div className="mt-6">
-                        <p className="text-[12px]">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry`s standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and
-                            scrambled it to make
-                        </p>
+                    <div
+                        id="item2"
+                        className="h-[208px] w-[465px] carousel-item px-10 pt-10 border border-gray-400 rounded-lg"
+                    >
+                        <div className="">
+                            <div className="flex">
+                                <div className="h-[70px] w-[70px]">
+                                    <img src="src\assets\Lolla 1.png" alt="" />
+                                </div>
+                                <div className="ml-6">
+                                    <h4 className="text[16px] font-bold">
+                                        Lolla Smith
+                                    </h4>
+                                    <p className="text-[14px]">Microsoft</p>
+                                    <p className="flex gap-2">
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <p className="text-[12px]">
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry`s standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        id="item3"
+                        className="h-[208px] w-[465px] carousel-item px-10 pt-10 border border-gray-400 rounded-lg"
+                    >
+                        <div className="">
+                            <div className="flex">
+                                <div className="h-[70px] w-[70px]">
+                                    <img src="src\assets\Lolla 1.png" alt="" />
+                                </div>
+                                <div className="ml-6">
+                                    <h4 className="text[16px] font-bold">
+                                        Lolla Smith
+                                    </h4>
+                                    <p className="text-[14px]">Microsoft</p>
+                                    <p className="flex gap-2">
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <p className="text-[12px]">
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry`s standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        id="item4"
+                        className="h-[208px] w-[465px] carousel-item px-10 pt-10 border border-gray-400 rounded-lg"
+                    >
+                        <div className="">
+                            <div className="flex">
+                                <div className="h-[70px] w-[70px]">
+                                    <img src="src\assets\Lolla 1.png" alt="" />
+                                </div>
+                                <div className="ml-6">
+                                    <h4 className="text[16px] font-bold">
+                                        Lolla Smith
+                                    </h4>
+                                    <p className="text-[14px]">Microsoft</p>
+                                    <p className="flex gap-2">
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                        <img src="src\assets\star.png" alt="" />
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="mt-6">
+                                <p className="text-[12px]">
+                                    Lorem Ipsum is simply dummy text of the
+                                    printing and typesetting industry. Lorem
+                                    Ipsum has been the industry`s standard dummy
+                                    text ever since the 1500s, when an unknown
+                                    printer took a galley of type and scrambled
+                                    it to make
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="h-[238px] w-[465px] px-10 pt-10 border border-gray-400 rounded-lg">
-                    <div className="flex">
-                        <div>
-                            <img src="src\assets\Lolla 1.png" alt="" />
-                        </div>
-                        <div className="ml-6">
-                            <h4 className="text[16px] font-bold">
-                                Lolla Smith
-                            </h4>
-                            <p className="text-[14px]">Microsoft</p>
-                            <p className="flex gap-2">
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                            </p>
-                        </div>
-                    </div>
-                    <div className="mt-6">
-                        <p className="text-[12px]">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry`s standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and
-                            scrambled it to make
-                        </p>
-                    </div>
-                </div>
-                <div className="h-[238px] w-[465px] px-10 pt-10 border border-gray-400 rounded-lg">
-                    <div className="flex">
-                        <div>
-                            <img src="src\assets\Lolla 1.png" alt="" />
-                        </div>
-                        <div className="ml-6">
-                            <h4 className="text[16px] font-bold">
-                                Lolla Smith
-                            </h4>
-                            <p className="text-[14px]">Microsoft</p>
-                            <p className="flex gap-2">
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                                <img src="src\assets\star.png" alt="" />
-                            </p>
-                        </div>
-                    </div>
-                    <div className="mt-6">
-                        <p className="text-[12px]">
-                            Lorem Ipsum is simply dummy text of the printing and
-                            typesetting industry. Lorem Ipsum has been the
-                            industry`s standard dummy text ever since the 1500s,
-                            when an unknown printer took a galley of type and
-                            scrambled it to make
-                        </p>
-                    </div>
+                <div className="flex justify-center w-full py-2 gap-2 mt-8">
+                    <a href="#item1" className="btn btn-xs">
+                        1
+                    </a>
+                    <a href="#item2" className="btn btn-xs">
+                        2
+                    </a>
+                    <a href="#item3" className="btn btn-xs">
+                        3
+                    </a>
+                    <a href="#item4" className="btn btn-xs">
+                        4
+                    </a>
                 </div>
             </div>
         </>
